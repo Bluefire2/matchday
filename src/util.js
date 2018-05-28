0213b3fce13f6c599a109ee1eec06550fcbc0179
Kyrylo Chernyshov <kc875@cornell.edu>
2018-05-28 13:37:51 -0400

    Optimise promise usage in getLeagueStandings() and getLeagueGames()

--- src/util.js
+++ src/util.js
@@ -1,9 +1,9 @@
 const constants = require('./constants'),
     LEAGUES = constants.LEAGUES;
 
-const parse = require('csv-parse'),
-    fs = require('fs'),
-    Promise = require('bluebird'),
+const Promise = require('bluebird'),
+    parse = Promise.promisify(require('csv-parse')),
+    fs = Promise.promisifyAll(require('fs')),
     moment = require('moment'),
     axios = require('axios'),
     cheerio = require('cheerio');
@@ -50,32 +50,28 @@ const leagueToStandingsURL = league => {
  */
 module.exports.getLeagueStandings = league => {
     const url = leagueToStandingsURL(league);
-    return new Promise((resolve, reject) => {
-        axios.get(url)
-            .then(response => {
-                const $ = cheerio.load(response.data),
-                    rows = $('.standings.has-team-logos tbody tr');
+    return axios.get(url)
+        .then(response => {
+            const $ = cheerio.load(response.data),
+                rows = $('.standings.has-team-logos tbody tr');
 
-                const standings = rows.map(function (i, elem) {
-                    const tds = $(this).find('td'),
-                        teamTd = $(tds[0]),
-                        team = $(teamTd).find('span.team-names').text(),
-                        goalDiffTd = $(tds[7]),
-                        goalDiff = parseInt($(goalDiffTd).text()),
-                        pointsTd = $(tds[8]),
-                        points = parseInt($(pointsTd).text());
-                    return {
-                        team,
-                        goalDiff,
-                        points
-                    };
-                }).get();
+            const standings = rows.map(function (i, elem) {
+                const tds = $(this).find('td'),
+                    teamTd = $(tds[0]),
+                    team = $(teamTd).find('span.team-names').text(),
+                    goalDiffTd = $(tds[7]),
+                    goalDiff = parseInt($(goalDiffTd).text()),
+                    pointsTd = $(tds[8]),
+                    points = parseInt($(pointsTd).text());
+                return {
+                    team,
+                    goalDiff,
+                    points
+                };
+            }).get();
 
-                resolve(standings);
-            }).catch(error => {
-            reject(error);
+            return standings;
         });
-    });
 };
 
 /**
@@ -85,41 +81,31 @@ module.exports.getLeagueStandings = league => {
  * @param daysAhead
  * @returns {Promise}
  */
-module.exports.getLeagueGames = (league, daysAhead = 7) => {
+module.exports.getLeagueGames = Promise.method(function (league, daysAhead = 7) {
     // to round up to the nearest day, we can add 1 day and then round down:
     const maxDate = moment().add(daysAhead + 1, 'days').startOf('day');
-    return new Promise((resolve, reject) => {
-        if (league in LEAGUES) {
-            const leagueID = leagueToID(LEAGUES[league]);
-            // for now, get data from the hardcoded .csv file
-            // TODO: change this and instead fetch live data from the server
-            const filepath = './data/spi_matches.csv';
-            fs.readFile(filepath, (error, csv) => {
-                if (error) {
-                    reject(error);
-                } else {
-                    parse(csv, {columns: true}, (csvError, output) => {
-                        if (csvError) {
-                            reject(csvError);
-                        } else {
-                            const matchesFromLeagueBeforeMaxDate =
-                                output
-                                    .filter(({league_id}) => parseInt(league_id) === leagueID) // filter league
-                                    .filter(({date}) => { // filter date
-                                        const d = moment(date, 'YYYY-MM-DD');
-                                        return d.isAfter(moment()) && d.isBefore(maxDate);
-                                    })
-                                    .map(({date, team1, team2, prob1, prob2, probtie, proj_score1, proj_score2}) => {
-                                        // keep only relevant properties
-                                        return {date, team1, team2, prob1, prob2, probtie, proj_score1, proj_score2};
-                                    });
-                            resolve(matchesFromLeagueBeforeMaxDate);
-                        }
+    if (league in LEAGUES) {
+        const leagueID = leagueToID(LEAGUES[league]);
+        // for now, get data from the hardcoded .csv file
+        // TODO: change this and instead fetch live data from the server
+        const filepath = './data/spi_matches.csv';
+        return fs.readFileAsync(filepath).then(csv => {
+            return parse(csv, {columns: true});
+        }).then(output => {
+            const matchesFromLeagueBeforeMaxDate =
+                output
+                    .filter(({league_id}) => parseInt(league_id) === leagueID) // filter league
+                    .filter(({date}) => { // filter date
+                        const d = moment(date, 'YYYY-MM-DD');
+                        return d.isAfter(moment()) && d.isBefore(maxDate);
+                    })
+                    .map(({date, team1, team2, prob1, prob2, probtie, proj_score1, proj_score2}) => {
+                        // keep only relevant properties
+                        return {date, team1, team2, prob1, prob2, probtie, proj_score1, proj_score2};
                     });
-                }
-            });
-        } else {
-            reject('Invalid league');
-        }
-    });
-};
+            return matchesFromLeagueBeforeMaxDate;
+        });
+    } else {
+        throw new TypeError('Invalid league');
+    }
+});
