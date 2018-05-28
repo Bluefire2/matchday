51fb73bb77594b9ed9108f6f16260c2289b6a6f0
Kyrylo Chernyshov <kc875@cornell.edu>
2018-05-28 12:58:02 -0400

    Calculate standings and games in matchday main function

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@
 const constants = require('./constants'),
     LEAGUES = constants.LEAGUES;
 
+const {getLeagueStandings, getLeagueGames} = require('./util');
+
 /**
  * Fetch the following:
  *  - The match data for all teams in [league] for the next [daysAhead] days
@@ -12,5 +14,15 @@ const constants = require('./constants'),
  * @param daysAhead
  */
 module.exports = (league, daysAhead = 7) => {
-    // empty for now
+    const standingsPromise = getLeagueStandings(league),
+        gamesPromise = getLeagueGames(league, daysAhead);
+
+    return Promise.all([standingsPromise, gamesPromise]).then(values => {
+        const standings = values[0],
+            games = values[1];
+
+        return new Promise((resolve, reject) => {
+            resolve({standings, games}); // for now don't do any calculations
+        });
+    });
 };
